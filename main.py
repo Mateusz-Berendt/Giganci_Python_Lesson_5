f499d412b5ce2c8a7a0b0ca0b5a2809ff5a5d705
Matthew <114170747+Mateusz-Berendt@users.noreply.github.com>
2022-10-23 09:05:36 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+MVP = int(input('Please enter number of marks: '))
+won = int(input('Please enter number of won games: '))
+lost = int(input('Please enter number of lost games: '))
+
+if won >= 10 and won > lost or MVP >= 7:
+  print('Huray! You can play!')
+else:
+  print('We are sorry, But you cant p
