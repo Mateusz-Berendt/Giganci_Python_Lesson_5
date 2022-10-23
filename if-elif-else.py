f7a0b4b620d3822f302f3d557877d3d75b0b01d6
Matthew <114170747+Mateusz-Berendt@users.noreply.github.com>
2022-10-23 09:05:53 +0200

    Create if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,8 @@
+num = int(input('Please enter the number: '))
+
+if num > 0:
+  print('The number is bigger than 0.')
+elif num < 0:
+  print('The number is smaller than 0.')
+else:
+  print('The number is 0.')
